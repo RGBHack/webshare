20991dfdc6e1c19fa8aaf3c8685546dc30121435
rjawesome <aonedtop@gmail.com>
2020-11-21 19:36:42 -0800

    fix more limits

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -55,7 +55,7 @@ export const getPosts = async (limit: number = 50) => {
 	}) as CardProps[]
 }
 
-export const usePosts = (def: CardProps[], limit: number = 10) => {
+export const usePosts = (def: CardProps[], limit: number = 50) => {
 	const [props, setProps] = useState(def)
 	useEffect(() => {
 		return firebase
